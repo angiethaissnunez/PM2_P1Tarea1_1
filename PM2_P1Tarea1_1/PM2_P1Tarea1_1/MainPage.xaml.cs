739e3355cf010703d171948e1c6c38a883d38285
angie nunez <angiethaiss52@gmail.com>
2022-10-19 18:54:06 -0600

    Add project files.

---
+++ PM2_P1Tarea1_1/PM2_P1Tarea1_1/MainPage.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PM2_P1Tarea1_1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnSuma_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtNum1.Text) || string.IsNullOrWhiteSpace(txtNum2.Text))
+            {
+                await DisplayAlert("ALERTA", "Debe llenar ambos campos ", "Ok");
+                return;
+            }
+            decimal num1 = decimal.Parse(txtNum1.Text);
+            decimal num2 = decimal.Parse(txtNum2.Text);
+            decimal result;
+
+            result = num1 + num2;
+            await Navigation.PushAsync(new Views.PageResult(result));
+
+
+        }
+
+        private async void btnResta_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtNum1.Text) || string.IsNullOrWhiteSpace(txtNum2.Text))
+            {
+                await DisplayAlert("ALERTA", "Debe llenar ambos campos ", "Ok");
+                return;
+            }
+            decimal num1 = decimal.Parse(txtNum1.Text);
+            decimal num2 = decimal.Parse(txtNum2.Text);
+            decimal result;
+
+            result = num1 - num2;
+            await Navigation.PushAsync(new Views.PageResult(result));
+        }
+
+        private async void btnDivision_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtNum1.Text) || string.IsNullOrWhiteSpace(txtNum2.Text))
+            {
+                await DisplayAlert("ALERTA", "Debe llenar ambos campos ", "Ok");
+                return;
+            }
+            decimal num1 = decimal.Parse(txtNum1.Text);
+            decimal num2 = decimal.Parse(txtNum2.Text);
+            decimal result;
+
+            result = num1 / num2;
+            await Navigation.PushAsync(new Views.PageResult(result));
+
+        }
+
+        private async void btnMultiplicacion_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtNum1.Text) || string.IsNullOrWhiteSpace(txtNum2.Text))
+            {
+                await DisplayAlert("ALERTA", "Debe llenar ambos campos ", "Ok");
+                return;
+            }
+            decimal num1 = decimal.Parse(txtNum1.Text);
+            decimal num2 = decimal.Parse(txtNum2.Text);
+            decimal result;
+
+            result = num1 * num2;
+            await Navigation.PushAsync(new Views.PageResult(result));
+
+        }
+
+        private void btnBorrar_Clicked(object sender, EventArgs e)
+        {
+            txtNum1.Text = "";
+            txtNum2.Text = "";
+
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+
+    }
+}
---
+++ PM2_P1Tarea1_1/PM2_P1Tarea1_1/Views/PageResult.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PM2_P1Tarea1_1.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageResult : ContentPage
+    {
+        public PageResult()
+        {
+            InitializeComponent();
+        }
+        public PageResult(decimal result)
+        {
+            InitializeComponent();
+            ResultadoOpe.Text = "El resultado es :" + result.ToString("0.##");
+        }
+    }
+}
